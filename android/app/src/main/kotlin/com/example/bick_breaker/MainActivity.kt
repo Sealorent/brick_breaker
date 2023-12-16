4fd7bc0ea34ca7e5e982603bfc9e54d04631bd8c
Sealorent <vikylorentseaputra@gmail.com>
2023-12-16 07:55:47 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/bick_breaker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bick_breaker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+import 'brick_breaker.dart';   
+
+import 'package:bick_breaker/widgets/game_app.dart';
+
+void main() {
+  runApp(
+    const GameApp()
+  );
+}
+
